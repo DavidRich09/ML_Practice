9faaef711a5e2253cb077936ec5d88b837395e3b
Luis Delgado <62965678+Tiquillo@users.noreply.github.com>
2021-08-25 12:11:12 -0600

    b

--- Regresión Polinomial.py
+++ Regresión Polinomial.py
@@ -6,31 +6,44 @@ from sklearn.preprocessing import PolynomialFeatures
 
 import matplotlib.pyplot as plt
 
-boston = datasets.load_boston()
+class RegresionPolinomial:
+    def __init__(self):
+        
+        self.boston = datasets.load_boston()
 
-X_p = boston.data[:, np.newaxis, 5]
-y_p = boston.target
+        self.X_p = self.boston.data[:, np.newaxis, 5]
+        self.y_p = self.boston.target
 
-plt.scatter(X_p, y_p)
-plt.show()
+    def GraficarNormal(self):
 
-X_train_p, X_test_p, y_train_p, y_test_p = train_test_split(X_p, y_p, test_size=0.65)
+        plt.scatter(self.X_p, self.y_p)
+        plt.title('Regresión Polinomial')
+        plt.xlabel('Número de habitaciones')
+        plt.ylabel('Valor medio')
+        plt.show()
 
-poli_reg = PolynomialFeatures(degree = 2)
+    def GraficaRegPol(self):
+        X_train_p, X_test_p, y_train_p, y_test_p = train_test_split(self.X_p, self.y_p, test_size=0.65)
 
-X_train_poli = poli_reg.fit_transform(X_train_p)
-X_test_poli = poli_reg.fit_transform(X_test_p)
+        poli_reg = PolynomialFeatures(degree = 2)
+
+        X_train_poli = poli_reg.fit_transform(X_train_p)
+        X_test_poli = poli_reg.fit_transform(X_test_p)
 
 
-pr = linear_model.LinearRegression()
-pr.fit(X_train_poli, y_train_p)
+        pr = linear_model.LinearRegression()
+        pr.fit(X_train_poli, y_train_p)
 
-Y_pred_pr = pr.predict(X_test_poli)
+        Y_pred_pr = pr.predict(X_test_poli)
 
-plt.scatter(X_test_p, y_test_p)
-plt.plot(X_test_p, Y_pred_pr, color='red', linewidth=3)
+        plt.scatter(X_test_p, y_test_p)
+        plt.plot(X_test_p, Y_pred_pr, color='red', linewidth=3)
 
-plt.title('Regresión Polinomial')
-plt.xlabel('Número de habitaciones')
-plt.ylabel('Valor medio')
-plt.show()
+        plt.title('Regresión Polinomial')
+        plt.xlabel('Distancia')
+        plt.ylabel('Precio')
+        plt.show()
+
+RP = RegresionPolinomial()
+RP.GraficarNormal()
+RP.GraficaRegPol()
