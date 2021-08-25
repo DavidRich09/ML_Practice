c23b314274014dc7b4fdc6da95f9d541e91d172d
Tiquillo <lujimenez2000@gmail.com>
2021-08-25 11:03:28 -0600

    vectores de soporte de regresión y regresión polinomial listos

---
+++ Regresión Polinomial.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+
+import matplotlib.pyplot as plt
+
+boston = datasets.load_boston()
+
+X_p = boston.data[:, np.newaxis, 5]
+y_p = boston.target
+
+X_train_p, X_test_p, y_train_p, y_test_p = train_test_split(X_p, y_p, test_size=0.2)
+
+poli_reg = PolynomialFeatures(degree = 2)
+
+X_train_poli = poli_reg.fit_transform(X_train_p)
+X_test_poli = poli_reg.fit_transform(X_test_p)
+
+
+pr = linear_model.LinearRegression()
+pr.fit(X_train_poli, y_train_p)
+
+Y_pred_pr = pr.predict(X_test_poli)
+
+plt.scatter(X_test_p, y_test_p)
+plt.plot(X_test_p, Y_pred_pr, color='red', linewidth=3)
+
+plt.title('Regresión Polinomial')
+plt.xlabel('Número de habitaciones')
+plt.ylabel('Valor medio')
+plt.show()
---
+++ Vectores de Soporte de Regresión.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn.svm import SVR
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+boston = datasets.load_boston()
+
+X_svr = boston.data[:, np.newaxis, 5]
+y_svr = boston.target
+
+plt.scatter(X_svr, y_svr)
+# plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X_svr, y_svr, test_size=0.2)
+
+svr = SVR(kernel='linear', C=1.0, epsilon=0.2)
+
+svr.fit(X_train, y_train)
+
+Y_pred = svr.predict(X_test)
+
+plt.scatter(X_test, y_test)
+plt.plot(X_test, Y_pred, color='red', linewidth=3)
+
+plt.title('MODELO VECTORES DE SOPORTE REGRESIÓN')
+plt.xlabel('Número de habitaciones')
+plt.ylabel('Valor medio')
+plt.show()
+
+print()
+print('DATOS DEL MODELO VECTORES DE SOPORTE REGRESIÓN')
+print()
+print('Precisión del modelo:')
+print(svr.score(X_train, y_train))
